a4ddf22914aab4cf72e90f7062e527e11da1915a
shawn.oakley <shawn.oakley@adminisatorsMBP.home>
2019-03-30 19:34:14 -0400

    Loads image and displays from mapbox

--- loadImage.js
+++ loadImage.js
@@ -1,4 +1,20 @@
 const utils = require('./utils');
 const fs = require('fs');
 
-conat MAPBOX_KEY = fs.readFileSync("mapbox.key").toString()
+const MAPBOX_KEY = fs.readFileSync("mapbox.key").toString()
+
+async function main() {
+    const zoom = 10;
+    const [lat, long] = [36.133487, -112.239884]
+
+    const tLat = Math.floor(utils.lat2tile(lat, zoom));
+    const tLong  = Math.floor(utils.long2tile(long, zoom));
+
+    const image = await utils.loadImage(
+        `https://api.mapbox.com/v4/mapbox.satellite/${zoom}/${tLong}/${tLat}.pngraw?access_token=${MAPBOX_KEY}`       
+    )
+
+    document.body.appendChild(image)
+}
+
+main();
